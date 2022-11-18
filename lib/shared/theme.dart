90785ebc97deed5ec0f419aa6d7dfeb0da602cd9
baihaqi43043 <baihaqi43043@gmail.com>
2022-11-18 22:24:03 +0700

    initial commit

---
+++ lib/shared/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Color kPrimaryColor = Color(0xFFE7CC);
+Color kBlueColor = Color(0x083AA9);
+Color kBlackColor = Color(0x101817);
+Color kGreyColor = Color(0x828A89);
+Color kGoldColor = Color(0xEAC92C);
+Color kWhiteColor = Color(0xFFFFFF);
+Color kBackgroundColor = Color(0xF9F9F9);
