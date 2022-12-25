734e560435817b5b52c46c7031584458547aff6c
baihaqi43043 <baihaqi43043@gmail.com>
2022-12-25 15:56:55 +0700

    prak 12 state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
-import 'ui/pages/splash_page.dart';
+import 'package:bagiundangan_app/views/shopping_page.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
+// This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SplashPage(),
+      title: 'Flutter GetX',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ShoppingPage(),
     );
   }
 }
