45d877dc30c631bed80919b0a78cacb79d1015d5
baihaqi43043 <baihaqi43043@gmail.com>
2022-12-25 13:40:10 +0700

    push_praktikum

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/material.dart';
-import 'ui/pages/splash_page.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
+import 'package:bagiundangan_app/home_page.dart';
+import 'package:bagiundangan_app/models/Task.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final applicatonDocumentDir =
+      await path_provider.getApplicationDocumentsDirectory();
+  Hive.init(applicatonDocumentDir.path);
+  Hive.registerAdapter(TaskAdapter());
+  await Hive.openBox<Task>('TODOs');
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashPage(),
+      title: 'Simple TASK',
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const MyHomePage(),
     );
   }
 }
